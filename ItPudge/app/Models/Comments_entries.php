d493486149949d511d22d9bfd3a1d80a642d25c7
crefise <crefise@gmail.com>
2021-06-01 18:28:32 +0300

    adding admin panel

--- ItPudge/app/Models/Comments_entries.php
+++ ItPudge/app/Models/Comments_entries.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments_entries extends Model
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
 
 
---
+++ ItPudge/routes/backpack/custom.php
@@ -0,0 +1,24 @@
+<?php
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => array_merge(
+        (array) config('backpack.base.web_middleware', 'web'),
+        (array) config('backpack.base.middleware_key', 'admin')
+    ),
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () { // custom admin routes
+    Route::crud('tag', 'TagCrudController');
+    Route::crud('user', 'UserCrudController');
+    Route::crud('post', 'PostCrudController');
+    Route::crud('like', 'LikeCrudController');
+    Route::crud('comments_entries', 'Comments_entriesCrudController');
+    Route::crud('comment', 'CommentCrudController');
+    Route::crud('category', 'CategoryCrudController');
+}); // this should be the absolute last line of this file
